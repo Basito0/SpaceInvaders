5489efab713a4e625aee1b2cd1a943d71e2bcd9a
Basito0 <76927670+Basito0@users.noreply.github.com>
2022-06-23 14:37:47 -0400

    Add files via upload

--- init2.c
+++ init2.c
@@ -31,7 +31,10 @@ SDL_Texture* LoadTexture(const char* filepath, SDL_Renderer* renderTarget){
 void NaveAvanzaIzqDer(Uint32 *dash, SDL_Rect *ship, int *vx, double *radio, double *delta);
 
 //funcion que hace que la nave dispare
-void NaveDispara(Uint32 *time, SDL_Rect *ship, SDL_Rect p_bullet[], int balas[1000][3],SDL_Renderer *renderer, SDL_Texture *texture);
+void NaveDispara(Uint32 *time, SDL_Rect *ship, SDL_Rect p_bullet[], int balas[10][3],SDL_Renderer *renderer, SDL_Texture *texture);
+
+//Spawnea aliens
+void AlienSpawn(SDL_Rect aliens[], int alieninfo[100][3]);
 
 int main(int argc, char* argv[]){
 	//si SDL no se inicia lanza una ventana de error
@@ -54,14 +57,14 @@ int main(int argc, char* argv[]){
 	SDL_Event event;//crea una variable de tipo evento
 	
 	/*
-Creamos un puntero que crea la ventana del juego, el nombre de la ventana es Space Invader, XSIZE es el ancho, definido como 600. YSIXE es el alto, definido como 600
-los dos primeros parametros son para establecer en que posicion se abre la venta, y el ultimo muestra la ventana
-renderer renderiza la imagen en la ventana
-*/		
+	Creamos un puntero que crea la ventana del juego, el nombre de la ventana es Space Invader, XSIZE es el ancho, definido como 600. YSIXE es el alto, definido como 600
+	los dos primeros parametros son para establecer en que posicion se abre la venta, y el ultimo muestra la ventana
+	renderer renderiza la imagen en la ventana
+	*/		
 	
 	window= SDL_CreateWindow("Space Invaders",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,XSIZE,YSIZE,SDL_WINDOW_SHOWN);
 	
-//Initialize SDL_mixer
+	//Initialize SDL_mixer
     if( Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) < 0){
         printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
     }
@@ -75,6 +78,7 @@ renderer renderiza la imagen en la ventana
 	//ACA ASIGNAMOS TEXTURAS Y COLISIONES
 	SDL_Texture* ship_texture = LoadTexture("Resources/Sprites/Nave_new.png", renderer);
 	SDL_Texture* b_Texture = LoadTexture("Resources/Sprites/Bullet_01.png", renderer);
+	SDL_Texture* al_Texture = LoadTexture("Resources/Sprites/alien.png", renderer);
 	SDL_Rect ship;
 
 	//Punteros para música
@@ -100,8 +104,12 @@ renderer renderiza la imagen en la ventana
 	ship.y = (int)(radio*cos(delta) + YSIZE/2);
 
 	//Balas del jugador, velocidad en x e y
-	int balas[1000][3];
-	SDL_Rect p_bullet[1000];
+	int balas[10][3];
+	SDL_Rect p_bullet[10];
+
+	//Balas del jugador, velocidad en x e y
+	int alieninfo[100][3];
+	SDL_Rect aliens[100];
 
 	//comienza un contador
 	Uint32 dashTime = SDL_GetTicks();
@@ -137,6 +145,9 @@ renderer renderiza la imagen en la ventana
 					//Play the music
         			Mix_PlayMusic(tema, -1 );
 				}
+				else if(keys[SDL_SCANCODE_DOWN]){ //movimiento hacia la derecha
+					AlienSpawn(aliens, alieninfo);
+				}
 			}		
 		}
 
@@ -144,7 +155,7 @@ renderer renderiza la imagen en la ventana
 		SDL_QueryTexture(ship_texture, NULL, NULL, &ship.w, &ship.h);
 
 		//Hace cosas con las balas del jugador
-		for (int i = 0; i < 1000; i++)
+		for (int i = 0; i < 10; i++)
 		{
 			if (balas[i][0] == 1)
 			{
@@ -155,6 +166,21 @@ renderer renderiza la imagen en la ventana
 			}
 			if (p_bullet[i].x < 0 || p_bullet[i].y < 0 || p_bullet[i].x > XSIZE || p_bullet[i].y > YSIZE){
 				balas[i][0] = 0;
+			}
+		}
+
+		//Hace cosas con los aliens
+		for (int i = 0; i < 100; i++)
+		{
+			if (alieninfo[i][0] == 1)
+			{
+				aliens[i].x += alieninfo[i][1]/10;
+				aliens[i].y += alieninfo[i][2]/10;
+				SDL_QueryTexture(al_Texture, NULL, NULL, &aliens[i].w, &aliens[i].h);
+				SDL_RenderCopy(renderer, al_Texture, NULL, &aliens[i]);
+			}
+			if (aliens[i].x < 0 || aliens[i].y < 0 || aliens[i].x > XSIZE || aliens[i].y > YSIZE){
+				alieninfo[i][0] = 0;
 			}
 		}
 
@@ -184,7 +210,7 @@ void NaveAvanzaIzqDer(Uint32 *dash, SDL_Rect *ship, int *vx, double *radio, doub
 
 	*delta += *vx*M_PI/45;
 
-	if(keys[SDL_SCANCODE_SPACE] && (tiempo_actual - *dash) > 2000){ //si se presiona el espacio y han pasado x segundos
+	if(keys[SDL_SCANCODE_SPACE] && (tiempo_actual - *dash) > 500){ //si se presiona el espacio y han pasado x segundos
 		*delta += *vx*M_PI/10;
 		*dash = tiempo_actual;
 	}
@@ -193,10 +219,10 @@ void NaveAvanzaIzqDer(Uint32 *dash, SDL_Rect *ship, int *vx, double *radio, doub
 }
 
 
-void NaveDispara(Uint32 *time, SDL_Rect *ship, SDL_Rect *p_bullet, int balas[1000][3], SDL_Renderer *renderer, SDL_Texture *texture){
+void NaveDispara(Uint32 *time, SDL_Rect *ship, SDL_Rect *p_bullet, int balas[10][3], SDL_Renderer *renderer, SDL_Texture *texture){
 
 	Uint32 tiempo_actual = SDL_GetTicks();
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		//1 significa inicializado. 0 es una bala disponible
 		if(balas[i][0] == 0 && (tiempo_actual - *time) > 500){
@@ -219,4 +245,21 @@ void NaveDispara(Uint32 *time, SDL_Rect *ship, SDL_Rect *p_bullet, int balas[100
 	}
 }
 
+void AlienSpawn(SDL_Rect *aliens, int alieninfo[100][3]){
+
+
+	for (int i = 0; i < 100; i++)
+	{
+		//1 significa inicializado. 0 es una bala disponible
+		if(alieninfo[i][0] == 0){
+			alieninfo[i][0] = 1;
+			aliens[i].x = XSIZE/2;
+			aliens[i].y = YSIZE/2;
+
+			//Calcula el movimiento del alien
+			alieninfo[i][1] = 1;
+			alieninfo[i][2] = 1;
+		}
+	}
+}
 //EXPLOOOOOSION
