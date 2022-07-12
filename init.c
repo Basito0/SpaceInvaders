5583e0442f376f268c0b4e7a4c832c3e51188081
elmismisimo5 <85262027+elmismisimo5@users.noreply.github.com>
2022-07-12 15:51:28 -0400

    Update init.c

--- init.c
+++ init.c
@@ -1,45 +1,23 @@
 //cambios de la megumin 
 #include <stdio.h>
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 #include <math.h>
 //crea variables globales
 //tamaño de ventana
-#define XSIZE 1280
-#define YSIZE 720
+#define XSIZE 600
+#define YSIZE 400
 //tiempo de espera en milisegundos
 #define MS 10
-//tamaño y velocidad de la nave
-#define NAVE_LEN 20
-#define NAVE_VEL 10
 //tamaño y velocidad del misil
 #define MISIL_LEN 5
 #define MISIL_VEL 5
-//se definen 2 estructuras
-//la estructura de tipo Nave con nombre Nave
-typedef struct Nave Nave;
-//la estructura de tipo Misil con nombre Misil
-typedef struct Misil Misil;
-//definimos que la figura de la nave esta compuesta por 3 puntos,
-//x1,y1,..., son las coordenadas de cada punto de la nave, la nave es un triangulo
-//por lo tanto, cada punto es un vertice
-//ademas vx y vy es la velocidad de la nave en el eje x y en el eje y respectivamente
-struct Nave{
-	int x1,y1;
-	int x2,y2;
-	int x3,y3;
-	int vx,vy;
-	Misil *misiles; //a la nave se le enlaza una lista de misiles para que pueda disparar	
-};
-//se crea la estructura del misil, los misiles son lineas, la posicion x1 y x2 definiran
-//el grosor del misil, y1,y2 el largo del misil
-struct Misil{
-	int x1,y1;
-	int x2,y2;
-	int vx,vy;
-	Misil *siguiente;//se le enlaza el misil siguiente
-};
+
+
+
 //las funciones estan definidas al final 
 
 //En resumen, carga una textura y la guarda en un puntero(?
@@ -51,23 +29,72 @@ SDL_Texture* LoadTexture(const char* filepath, SDL_Renderer* renderTarget){
 
 	return texture;
 }
-//esta funcion elimina los misiles que salgan de la pantalla que estan ocupando memoria dinamica
-//recibe los misiles
-void MisilBorraObsoletos(Misil *misiles);
 
-void NaveAvanzaArrAba(Nave *nave, SDL_Rect *ship);
-
-void NaveAvanzaIzqDer(Nave *nave, SDL_Rect *ship);
-//esta funcion hace que los misiles avancen, anvanza por el eje y
-void MisilAvanza(Misil *misil);
-
-//funcion que pinta el borde de la nave (dibuja el triangulo)
-void NavePinta(Nave *nave, SDL_Renderer *renderer);
+//Función que mueve la nave de forma circunferencial
+void NaveAvanzaIzqDer(Uint32 *dash, SDL_Rect *ship, int *vx, double *radio, double *delta);
 
 //funcion que hace que la nave dispare
-void NaveDispara(Nave *nave);
+void NaveDispara(Uint32 *time, SDL_Rect *ship, SDL_Rect p_bullet[], int balas[10][3],SDL_Renderer *renderer, SDL_Texture *texture);
+
+//Spawnea aliens
+void AlienSpawn(SDL_Rect *ship, SDL_Rect *aliens, double alieninfo[100][6], Uint32 *spawntime, int radio);
 
 int main(int argc, char* argv[]){
+	    int gameOver;
+    const SDL_MessageBoxButtonData buttons[] = {
+        { /* .flags, .buttonid, .text */        0, 0, "Jugar" },
+        { SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 1, "Instrucciones" },
+        { SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 2, "Salir" },
+        {3,3,"Hola"}
+    };
+    const SDL_MessageBoxColorScheme colorScheme = {
+        { /* .colors (.r, .g, .b) */
+            /* [SDL_MESSAGEBOX_COLOR_BACKGROUND] */
+            { 0,   0,   0 },
+            /* [SDL_MESSAGEBOX_COLOR_TEXT] */
+            {   255, 255,   255 },
+            /* [SDL_MESSAGEBOX_COLOR_BUTTON_BORDER] */
+            { 255, 255,   255 },
+            /* [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] */
+            {   0,   0, 0 },
+            /* [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] */
+            { 255,   0, 255 }
+        }
+    };
+    const SDL_MessageBoxData messageboxdata = {
+        SDL_MESSAGEBOX_INFORMATION, /* .flags */
+        NULL, /* .window */
+        "Space Invaders", /* .title */
+        "Selecciona una opción", /* .message */
+        SDL_arraysize(buttons), /* .numbuttons */
+        buttons, /* .buttons */
+        &colorScheme /* .colorScheme */
+    };
+
+
+    int buttonid;
+    if (SDL_ShowMessageBox(&messageboxdata, &buttonid) < 0) {
+        SDL_Log("error displaying message box");
+        return 1;
+    }
+    if (buttonid == -1) {
+        SDL_Log("no selection");
+    } else if(buttonid == 0){
+        gameOver = 0;
+        printf("%d\n",gameOver);
+    } else if(buttonid == 1){        
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION,"Space Invaders","Instrucciones",NULL);
+    } else if(buttonid == 2){
+        return 0;
+    } else if(buttonid == 3){
+    	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION,"HOLA","hola",NULL);
+    	return 0;
+    }
+
+
+    else {
+        SDL_Log("selection was %s", buttons[buttonid].text);
+    }
 	//si SDL no se inicia lanza una ventana de error
 	if(SDL_Init(SDL_INIT_EVERYTHING)<0){
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"Error",SDL_GetError(),NULL);
@@ -79,7 +106,7 @@ int main(int argc, char* argv[]){
 	}
 	
 	int typeEvent;//crea una variable entera typeEvent para almacenar la entrada de tecla
-	int gameOver;//crea una variable entera para controlar si sigue dentro del ciclo del juego o no 
+	//crea una variable entera para controlar si sigue dentro del ciclo del juego o no 
 	
 	const unsigned char *keys;//crea una constante char sin signo para identificar la tecla que se pulsa
 	
@@ -87,185 +114,262 @@ int main(int argc, char* argv[]){
 	SDL_Renderer *renderer;//crea un puntero de tipo Render con nombre renderer
 	SDL_Event event;//crea una variable de tipo evento
 	
-	//**
-	//aqui le damos las dimensiones a la nave, el ultimo parametro NULL es que no tiene misiles
-	Nave nave ={XSIZE/2,YSIZE/2,XSIZE/2-NAVE_LEN,YSIZE/2+NAVE_LEN,XSIZE/2+NAVE_LEN,YSIZE/2+NAVE_LEN,NAVE_VEL,NAVE_VEL,NULL};
-/*
-Creamos un puntero que crea la ventana del juego, el nombre de la ventana es Space Invader, XSIZE es el ancho, definido como 600. YSIXE es el alto, definido como 600
-los dos primeros parametros son para establecer en que posicion se abre la venta, y el ultimo muestra la ventana
-renderer renderiza la imagen en la ventana
-*/		
+	/*
+	Creamos un puntero que crea la ventana del juego, el nombre de la ventana es Space Invader, XSIZE es el ancho, definido como 600. YSIXE es el alto, definido como 600
+	los dos primeros parametros son para establecer en que posicion se abre la venta, y el ultimo muestra la ventana
+	renderer renderiza la imagen en la ventana
+	*/		
 	
 	window= SDL_CreateWindow("Space Invaders",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,XSIZE,YSIZE,SDL_WINDOW_SHOWN);
 	
-	renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+	//Initialize SDL_mixer
+    if( Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) < 0){
+        printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+    }
+
+	renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	
-	gameOver=0;//le asigna 0 a la variable gameOver (0 es Falso)
+	//le asigna 0 a la variable gameOver (0 es Falso)
 	
 	keys=SDL_GetKeyboardState(NULL);//se le asigna a keys la funcion GetKeyboardState, que detecta que tecla fue pulsada, la tecla presionada se guarda en key
 	
+	//ACA ASIGNAMOS TEXTURAS Y COLISIONES
 	SDL_Texture* ship_texture = LoadTexture("Resources/Sprites/Nave_new.png", renderer);
+	SDL_Texture* b_Texture = LoadTexture("./Resources/Sprites/Bullet_01.png", renderer);
+	SDL_Texture* al_Texture = LoadTexture("Resources/Sprites/Alien1_1.png", renderer);
 	SDL_Rect ship;
 
-	//pide la posición inicial de la nave (cambiar xsize)
-	float x_pos = XSIZE;
+	//Punteros para música
+	Mix_Music *tema = NULL;
+
+	//Para efectos de sonido
+	Mix_Chunk *gScratch = NULL;
+	Mix_Chunk *gHigh = NULL;
+	Mix_Chunk *gMedium = NULL;
+	Mix_Chunk *gLow = NULL;
+
+	tema = Mix_LoadMUS("Resources/Audio/Music/Long_live_the_new_fresh.wav");
+
+	//Variables para el movimiento de la nave
+	float ox, oy;
+	double radio = 200;
+	ox = (XSIZE - ship.w)/2 + radio;
+	oy = (YSIZE - ship.h)/2 + radio;
+	double delta = asin(0);
+
 	//Posiciona el sprite de la nave
-	ship.x = ((XSIZE - ship.w) /2);
-	ship.y = ((YSIZE - ship.h) /2);
+	ship.x = (int)(radio*sin(delta) + XSIZE/2);
+	ship.y = (int)(radio*cos(delta) + YSIZE/2);
+
+	//Balas del jugador, velocidad en x e y
+	int balas[10][3];
+	SDL_Rect p_bullet[10];
+
+	//Aliens y su info
+	//[x][0] indica inicialización
+	//[x][1] indica el tipo de movimiento (1= recto, 2= espiral)
+	//[x][2] variable ángulo
+	//[x][3] variable radio
+	//[x][4] Velocidad inicial x
+	//[x][5] Velocidad inicial y
+	double alieninfo[100][6]; for(int i=0; i<100; i++){alieninfo[i][0] = 0;}
+	SDL_Rect aliens[100];
+
+	//comienza un contador
+	srand(SDL_GetTicks());
+	Uint32 dashTime = SDL_GetTicks();
+	Uint32 bulletTime = SDL_GetTicks();
+	Uint32 spawnTime = SDL_GetTicks();
+	
 
 
-	while(!gameOver){//se niega gameOver para que se considere verdadera, mientras sea "verdadera" tb se puede usar la condicion gameOver==0
+	while(gameOver == 0){//se niega gameOver para que se considere verdadera, mientras sea "verdadera" tb se puede usar la condicion gameOver==0
+		
+		//RenderClear limpia la ventana con un color X
+		SDL_RenderClear(renderer);
 		if(SDL_PollEvent(&event)){ //si se detecta la pulsacion de una tecla
 			typeEvent=event.type; //ve que tipo de tecla se pulso
 			if(typeEvent== SDL_QUIT){ //si se presiono la x que cierra la ventana
 				gameOver=1; //termina el juego
-			}else if(typeEvent==SDL_MOUSEBUTTONDOWN){ //si se detecta la pulsacion del mouse
+			}
+			else if(typeEvent==SDL_MOUSEBUTTONDOWN){ //si se detecta la pulsacion del mouse
 				if(SDL_GetMouseState(NULL,NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)){ //si es el clicl izq
-					NaveDispara(&nave); //la nave dispara
+					NaveDispara(&bulletTime, &ship, p_bullet, balas, renderer, b_Texture); //la nave dispara
 				}
 				
-			}else if(typeEvent == SDL_KEYDOWN){ //si se detecta la pulsacion de una tecla
+			}
+			else if(typeEvent == SDL_KEYDOWN){ //si se detecta la pulsacion de una tecla
 				if(keys[SDL_SCANCODE_ESCAPE]){	//guarda el tipo de tecla en keys
 					gameOver=1; //si la tecla es ESC el juego termina
 				}else if(keys[SDL_SCANCODE_LEFT]){ //si es la flecha izquierda, la nave se mueve hacia la izquierda
-					nave.vx=-abs(nave.vx); //la velocidad de la nave en el eje x se vuelve negativa
-					NaveAvanzaIzqDer(&nave, &ship); //la nave se mueve
+					int vx = -1; //la velocidad de la nave en el eje x se vuelve negativa
+					NaveAvanzaIzqDer(&dashTime, &ship, &vx, &radio, &delta); //la nave se mueve
 				}else if(keys[SDL_SCANCODE_RIGHT]){ //movimiento hacia la derecha
-					nave.vx=+abs(nave.vx); //la velocidad de la nave se hace positiva
-					NaveAvanzaIzqDer(&nave, &ship); //la nave se mueve
-				}else if(keys[SDL_SCANCODE_SPACE]){ //si se presiona el espacio
-					NaveDispara(&nave); //la nave dispara
-				}else if(keys[SDL_SCANCODE_UP]){ //la nave sube
-					nave.vy=-abs(nave.vy); //para que la nave suba, la velocidad en el eje y debe ser negativa, ni idea pq
-					NaveAvanzaArrAba(&nave, &ship); //la nave sube
-				}else if(keys[SDL_SCANCODE_DOWN]){ //la nave baja
-					nave.vy=+abs(nave.vy); //para que baje la aceleracion debe ser positiva
-					NaveAvanzaArrAba(&nave, &ship); //la nave baja
+					int vx = 1; //la velocidad de la nave se hace positiva
+					NaveAvanzaIzqDer(&dashTime, &ship, &vx, &radio, &delta); //la nave se mueve
+				}else if(keys[SDL_SCANCODE_SPACE] || keys[SDL_SCANCODE_UP]){ //si se presiona el espacio
+					NaveDispara(&bulletTime, &ship, p_bullet, balas, renderer, b_Texture); //la nave dispara
+				}
+
+				//COMANDOS DE DESARROLLO
+				else if(keys[SDL_SCANCODE_UP]){ //Empieza musica
+					//Play the music
+        			Mix_PlayMusic(tema, -1 );
 				}
 			}		
 		}
-		MisilBorraObsoletos(nave.misiles); // luego de cada movimiento, se borran los misiles que no aparezcan en pantalla
-		//pantalla
-
+		//Aparece un alien cada x milisegundos
+		AlienSpawn(&ship, aliens, alieninfo, &spawnTime, radio);
 
 		//Pide info de texturas
 		SDL_QueryTexture(ship_texture, NULL, NULL, &ship.w, &ship.h);
 
-		SDL_SetRenderDrawColor(renderer,0,0,0,0); //pinta la pantalla negra
-		SDL_RenderClear(renderer);
+		//Hace cosas con las balas del jugador
+		for (int i = 0; i < 10; i++)
+		{
+			if (balas[i][0] == 0){
+				p_bullet[i].x = XSIZE;
+				p_bullet[i].y = YSIZE;
+			}
+			if (balas[i][0] == 1)
+			{
+				p_bullet[i].x += balas[i][1]/10;
+				p_bullet[i].y += balas[i][2]/10;
+				SDL_QueryTexture(b_Texture, NULL, NULL, &p_bullet[i].w, &p_bullet[i].h);
+				SDL_RenderCopy(renderer, b_Texture, NULL, &p_bullet[i]);
+			}
+			if (p_bullet[i].x < 0 || p_bullet[i].y < 0 || p_bullet[i].x > XSIZE || p_bullet[i].y > YSIZE){
+				balas[i][0] = 0;
+			}
+		}
+
+		//Hace cosas con los aliens
+		for (int i = 0; i < 100; i++)
+		{
+			if (alieninfo[i][0] == 0){
+				aliens[i].x = 0;
+				aliens[i].y = 0;
+			}
+			if (alieninfo[i][0] == 1)
+			{
+				for(int u=0; u<10; u++){
+					if(SDL_HasIntersection(&p_bullet[u], &aliens[i]) == SDL_TRUE){
+						alieninfo[i][0] = 0;
+						balas[u][0] = 0;
+					}
+				}
+				if(SDL_HasIntersection(&ship, &aliens[i]) == SDL_TRUE){
+					alieninfo[i][0] = 0;
+				}
+
+				if(alieninfo[i][1] == 1){
+					aliens[i].x += alieninfo[i][4];
+					aliens[i].y += alieninfo[i][5];
+				}
+				if(alieninfo[i][1] == 2){
+					alieninfo[i][3] += (0.1);
+					alieninfo[i][2] += 1*M_PI/120;
+					aliens[i].x = (int)(alieninfo[i][3]*sin(alieninfo[i][2]) + (XSIZE - aliens->w)/2);
+					aliens[i].y = (int)(alieninfo[i][3]*cos(alieninfo[i][2]) + (YSIZE - aliens->h)/2);
+				}
+				SDL_QueryTexture(al_Texture, NULL, NULL, &aliens[i].w, &aliens[i].h);
+				SDL_RenderCopy(renderer, al_Texture, NULL, &aliens[i]);
+			}
+			if (aliens[i].x < 0 || aliens[i].y < 0 || aliens[i].x > XSIZE || aliens[i].y > YSIZE){
+				alieninfo[i][0] = 0;
+			}
+		}
+
 		SDL_RenderCopy(renderer, ship_texture, NULL, &ship);
-		//pinta la nave de blanco
-		SDL_SetRenderDrawColor(renderer,255,255,255,0);
-		NavePinta(&nave,renderer);
 		//presenta la imagen, creo
 		SDL_RenderPresent(renderer);
-		SDL_Delay(MS);		
+		SDL_Delay(MS);	
 	}
 	
 	//finaliza todo
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+	Mix_Quit();
 	SDL_Quit();
 	
 	return 0;
 }
 
-
-void NavePinta(Nave *nave, SDL_Renderer *renderer){
-	//pinta misiles
-	Misil *ixMisil=nave->misiles;
-	while(ixMisil!=NULL){
-		MisilAvanza(ixMisil);
-		SDL_RenderDrawLine(renderer,ixMisil->x1,ixMisil->y1,ixMisil->x2,ixMisil->y2);
-		ixMisil=ixMisil->siguiente;
-	}
-	//pinta nave
-	SDL_Point points[4]={
-		{nave->x1,nave->y1},
-		{nave->x2,nave->y2},
-		{nave->x3,nave->y3},
-	};
-	//para pintar la nave en vez de recorrer 3 puntos recorre 4, para dibujar el borde completo del triangulo
-	points[3]=points[0];
-	SDL_RenderDrawLines(renderer,points,4);//pinta el triangulo
-	
-
-}
-
 //para moverse de izquierda a derecha, dependera de la velocidad en el eje x
 //si la velocidad en el eje x es negativa se mueve hacia la izquierda
 //en caso contrario se mueve hacia la derecha
-double a = 0;
-void NaveAvanzaIzqDer(Nave *nave, SDL_Rect *ship){
-	ship->x += nave->vx;
-	nave->x1+=nave->vx;
-	nave->x2+=nave->vx;
-	nave->x3+=nave->vx;
+void NaveAvanzaIzqDer(Uint32 *dash, SDL_Rect *ship, int *vx, double *radio, double *delta){
+
+	Uint32 tiempo_actual = SDL_GetTicks();
+	const unsigned char *keys;
+	keys=SDL_GetKeyboardState(NULL);
+
+	*delta += *vx*M_PI/45;
+
+	if(keys[SDL_SCANCODE_SPACE] && (tiempo_actual - *dash) > 500){ //si se presiona el espacio y han pasado x segundos
+		*delta += *vx*M_PI/10;
+		*dash = tiempo_actual;
+	}
+	ship->x = (int)(*radio*sin(*delta) + (XSIZE - ship->w)/2);
+	ship->y = (int)(*radio*cos(*delta) + (YSIZE - ship->h)/2);
 }
-//ocurre lo mismo que en el movimiento horizontal, solo que si la velocidad es negativa
-//la nave sube, y si es positiva la nave baja
-void NaveAvanzaArrAba(Nave *nave, SDL_Rect *ship){
-	ship->y += nave->vy;
-	nave->y1+=nave->vy;
-	nave->y2+=nave->vy;
-	nave->y3+=nave->vy;
-}
-//en resumen, en esta parte se crea un arreglo de misiles con memoria dinamica
-//el primer if verifica si la nave tiene misiles ir a (** mas arriba), la configure para que parta sin misiles
-//luego, en el else, si no tiene misiles, los crea y los guarda en la nave
-void NaveDispara(Nave *nave){
-	if(nave->misiles==NULL){
-		nave->misiles=(Misil *)SDL_malloc(sizeof(Misil));
-		nave->misiles->x1=nave->x1;
-		nave->misiles->y1=nave->y1;
-		nave->misiles->x2=nave->x1;
-		nave->misiles->y2=nave->y1 -MISIL_LEN;
-		nave->misiles->vx=0;
-		nave->misiles->vy=-MISIL_VEL;
-		nave->misiles->siguiente=NULL;
-	}else{
-		Misil *ixMisil =nave->misiles;
-		while(ixMisil->siguiente!=NULL){
-			ixMisil=ixMisil->siguiente;	
+
+
+void NaveDispara(Uint32 *time, SDL_Rect *ship, SDL_Rect *p_bullet, int balas[10][3], SDL_Renderer *renderer, SDL_Texture *texture){
+
+	Uint32 tiempo_actual = SDL_GetTicks();
+	for (int i = 0; i < 10; i++)
+	{
+		//1 significa inicializado. 0 es una bala disponible
+		if(balas[i][0] == 0 && (tiempo_actual - *time) > 500){
+			balas[i][0] = 1;
+			p_bullet[i].x = ship->x;
+			p_bullet[i].y = ship->y;
+
+			//Calcula la velocidad de la bala
+			balas[i][1] = -1*(p_bullet[i].x - (XSIZE/2));
+			balas[i][2] = -1*(p_bullet[i].y - (YSIZE/2));
+
+			//Dibuja la bala en pantalla
+			SDL_QueryTexture(texture, NULL, NULL, &p_bullet[i].w, &p_bullet[i].h);
+			SDL_RenderCopy(renderer, texture, NULL, &p_bullet[i]);
+
+			//reinicia time
+			*time = tiempo_actual;
+			break;
 		}
-		ixMisil->siguiente=(Misil *)SDL_malloc(sizeof(Misil));
-		ixMisil=ixMisil->siguiente;
-		ixMisil->x1=nave->x1;
-		ixMisil->y1=nave->y1;
-		ixMisil->x2=nave->x1;
-		ixMisil->y2=nave->y1-MISIL_LEN;
-		ixMisil->vx=0;
-		ixMisil->vy=-MISIL_VEL;
-		ixMisil->siguiente=NULL;
 	}
 }
 
-//esto hace que los misiles se muevan hacia arriba, la velocidad siempre es negativa
-//parten de la posicion de la nave
-void MisilAvanza(Misil *misil){
-	misil->x1+=misil->vx;
-	misil->y1+=misil->vy;
-	misil->x2+=misil->vx;
-	misil->y2+=misil->vy;
-}
-//si no hay misiles no hace nada
-//mientras hayan misiles, si estan fuera de la pantalla los borra
-//en caso contrario no hace nada
-//creo que era asi
-void MisilBorraObsoletos(Misil *misiles){
-	Misil * ixMisil=misiles;
-	if(ixMisil==NULL){
-		return;
-	}
-	Misil *auxMisil;
-	while(ixMisil->siguiente!=NULL){
-		if(ixMisil->siguiente->y1<0){
-			auxMisil=ixMisil->siguiente->siguiente;
-			SDL_free(ixMisil->siguiente);
-			ixMisil->siguiente=auxMisil;
-		}else{
-			ixMisil=ixMisil->siguiente;
+void AlienSpawn(SDL_Rect *ship, SDL_Rect *aliens, double alieninfo[100][6], Uint32 *spawntime, int radio){
+
+	Uint32 tiempo_actual = SDL_GetTicks();
+	int tipo = rand() % 101;
+
+	for (int i = 0; i < 20; i++)
+	{
+		//1 significa inicializado. 0 es una bala disponible
+		if(alieninfo[i][0] == 0 && tiempo_actual - *spawntime > 100){
+			alieninfo[i][0] = 1;
+			aliens[i].x = XSIZE/2;
+			aliens[i].y = YSIZE/2 - 1;
+			if(tipo <= 50){
+				alieninfo[i][1] = 1;
+				if((ship->x - (XSIZE + ship->w)/2)/20 > radio && (ship->y - (YSIZE + ship->h)/2)/20 > radio - 50){
+					alieninfo[i][4] = (ship->x - (XSIZE + ship->w)/2)/20;
+					alieninfo[i][5] = (ship->y - (YSIZE + ship->h)/2)/20;
+				}
+			}
+			if(tipo > 50){				
+				alieninfo[i][1] = 2;
+				alieninfo[i][2] = 0;
+				alieninfo[i][3] = 1;
+				
+			}
+
+			*spawntime = tiempo_actual;
+			break;
 		}
 	}
 }
-//EXPLOOOOOOOSION
+//EXPLOOOOOSION
